35831cc57952e472f32a4cc782cbcd8cce785fbc
Josua Schmid <josua.schmid@renuo.ch>
2016-10-23 15:26:09 +0200

    Set up config and tests

---
+++ spec/lib/panel_spec.rb
@@ -0,0 +1,15 @@
+require 'panel'
+
+RSpec.describe Panel do
+  subject { described_class.new }
+
+  it 'initializes with no buttons pressed' do
+    expect(subject.up?).to be_falsey
+    expect(subject.down?).to be_falsey
+  end
+
+  it 'can press the buttons' do
+    expect { subject.up! }.to change { subject.up? }.from(false).to(true)
+    expect { subject.down! }.to change { subject.down? }.from(false).to(true)
+  end
+end
