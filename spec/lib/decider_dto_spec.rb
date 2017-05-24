2ed5569e9916a8cb7c7aeaf9647f4eec6380e429
Josua Schmid <josua.schmid@renuo.ch>
2017-05-24 07:09:32 +0200

    Test dto for format

---
+++ spec/lib/decider_dto_spec.rb
@@ -0,0 +1,32 @@
+require 'decider_dto'
+require 'elevator'
+require 'floor'
+
+RSpec.describe DeciderDto do
+  describe '#new' do
+    let(:instance) { DeciderDto.new(elevator, panels) }
+    let(:elevator) { Elevator.new(double(:empty_decider)) }
+    let(:panels) { [Panel.new] }
+
+    context 'for elevator' do
+      subject { instance.elevator }
+
+      it 'builds transfer object' do
+        expect(subject.capacity).to be_a(Numeric)
+        expect(subject.current_floor).to be_a(Numeric)
+        expect(subject.target_floors).to be_an(Enumerable)
+        expect(subject.statistics).to be_a(Numeric)
+      end
+    end
+
+    context 'for elevator' do
+      subject { instance.floors }
+
+      it 'builds transfer object for floors' do
+        expect(subject).to be_an(Enumerable)
+        expect(subject.first.panel.up).to be(false)
+        expect(subject.first.panel.down).to be(false)
+      end
+    end
+  end
+end
